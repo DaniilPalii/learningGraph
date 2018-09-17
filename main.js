fd3e439cacec52c29b7ccd37837621613684956c
DaniilPalii <palii.daniil.o@gmail.com>
2018-09-17 22:57:08 +0200

    Clear repo

--- main.js
+++
@@ -1,8 +0,0 @@
-const { app, BrowserWindow } = require('electron');
-  
-function createWindow () {
-  win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadFile('sources/views/index.html');
-}
-
-app.on('ready', createWindow);
