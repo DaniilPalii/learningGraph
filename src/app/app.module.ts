c14a5ac94dee797bd65d3dbc9a4ad4b8f6ee6db2
DaniilPalii <palii.daniil.o@gmail.com>
2018-09-30 21:50:25 +0200

    Create base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationPanelComponent } from './navigation-panel/navigation-panel.component';
+import { MainPanelComponent } from './main-panel/main-panel.component';
+import { ActionPanelComponent } from './main-panel/action-panel/action-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationPanelComponent,
+    MainPanelComponent,
+    ActionPanelComponent
   ],
   imports: [
     BrowserModule
