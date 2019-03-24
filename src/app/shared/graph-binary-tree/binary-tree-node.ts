40e3597e0fd53bc654dbaf01f3b5530be10ee082
DaniilPalii <palii.daniil.o@gmail.com>
2019-03-24 21:58:00 +0100

    Change drawing placement algorithm

--- src/app/shared/graph-binary-tree/binary-tree-node.ts
+++ src/app/shared/graph-binary-tree/binary-tree-node.ts
@@ -2,7 +2,7 @@ export class BinaryTreeNode {
   public value: number = null;
   public parent: BinaryTreeNode = null;
   public children = new Array<BinaryTreeNode>();
-  public childCount: number = 0;
+  // public childCount: number = 0;
   public leftChild: BinaryTreeNode = null;
   public rightChild: BinaryTreeNode = null;
   public level: number = 1;
@@ -36,29 +36,31 @@ export class BinaryTreeNode {
     leftChild is ${this.leftChild ? 'present' : 'empty'},
     rightChild is ${this.rightChild ? 'present' : 'empty'},
     level: ${this.level},
-    childCount: ${this.childCount}`;
+    childCount: ${this.children.length}`;
   }
 
-  private appendLeftChild(futureChild: BinaryTreeNode): void {
-    this.leftChild = futureChild;
-    this.afterAppendingChild(futureChild);
+  private appendLeftChild(child: BinaryTreeNode): void {
+    this.leftChild = child;
+    this.afterAppendingChild(child);
   }
 
-  private appendRightChild(futureChild: BinaryTreeNode): void {
-    this.rightChild = futureChild;
-    this.afterAppendingChild(futureChild);
+  private appendRightChild(child: BinaryTreeNode): void {
+    this.rightChild = child;
+    this.afterAppendingChild(child);
   }
 
-  private afterAppendingChild(futureChild: BinaryTreeNode): void {
-    futureChild.parent = this;
-    this.childCount = futureChild.childCount + 1;
+  private afterAppendingChild(child: BinaryTreeNode): void {
+    child.parent = this;
+    // this.childCount = futureChild.childCount + 1; //todo ?
     this.refreshChildrenList();
     this.updateChildrenLevelRecursively();
   }
 
   private updateChildrenLevelRecursively(): void {
+    const childrenLevel = this.level + 1;
+
     this.children.forEach(child => {
-      child.level = this.level + 1;
+      child.level = childrenLevel;
       child.updateChildrenLevelRecursively();
     });
   }
@@ -66,8 +68,8 @@ export class BinaryTreeNode {
   private refreshChildrenList(): void {
     const children = new Array<BinaryTreeNode>();
 
-    if (this.leftChild) this.children.push(this.leftChild);
-    if (this.rightChild) this.children.push(this.rightChild);
+    if (this.leftChild) children.push(this.leftChild);
+    if (this.rightChild) children.push(this.rightChild);
 
     this.children = children;
   }
